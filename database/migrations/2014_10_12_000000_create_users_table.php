77a1dda2c22232bfc003b84363bee1fd7eae86fe
dangbing <bing.dang@zebra-c.com>
2018-03-14 17:00:16 +0800

    'users';

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('email',100)->unique();
+            $table->string('password',60);
             $table->rememberToken();
             $table->timestamps();
         });
