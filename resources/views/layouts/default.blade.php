4b71b940a4a341dd06033835bcaa58d3091d01eb
dangbing <bing.dang@zebra-c.com>
2018-03-14 16:10:40 +0800

    'style';

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title','Blog')</title>
+    <link rel="stylesheet" href="/css/app.css?123123">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+
+
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
+</div>
+
+
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home_');
+Route::get('/help', 'StaticPagesController@help')->name('help_');
+Route::get('/about', 'StaticPagesController@about')->name('about_');
+Route::get('signup', 'UsersController@create')->name('signup_');
