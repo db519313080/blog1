fd0d194d8a24f6858ca01e6cc27573a7fc5af34f
dangbing <bing.dang@zebra-c.com>
2018-04-20 17:09:22 +0800

    微博功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,7 +28,8 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()->orderBy('created_at','desc')->paginate(30);
+        return view('users.show', compact('user','statuses'));
     }
 
     //注册
--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,8 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
-
-
+//微博
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
 
 
 //Test
